52f3391f9626c99f0a3719233519bedce91b5e63
p2pfans <977466652@qq.com>
2020-05-04 23:12:09 +0800

    add name node

---
+++ src/main/java/com/toipr/mapper/node/NameNodeMapper.java
@@ -0,0 +1,7 @@
+package com.toipr.mapper.node;
+
+/**
+ * Created by DELL on 2020/5/4.
+ */
+public interface NameNodeMapper {
+}
---
+++ src/main/java/com/toipr/model/node/NameNode.java
@@ -0,0 +1,7 @@
+package com.toipr.model.node;
+
+/**
+ * Created by DELL on 2020/5/4.
+ */
+public class NameNode {
+}
